edae34fd91e5fb54a4f2499f7216e0adbc0b4317
fumiphys <ufimihot2703@gmail.com>
2018-09-09 15:02:23 +0900

    setting.sh: fix bug of equal operator

--- setting.sh
+++ setting.sh
@@ -8,11 +8,11 @@
 # if you use mac, brew will be used.
 # if you use linux, apt will be used.
 # if you use windows, I will think it later.
-if [ "$(uname)" == 'Darwin' ]; then
+if [ "$(uname)" = 'Darwin' ]; then
 	OS="Mac"
-elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+elif [ "$(expr substr $(uname -s) 1 5)" = 'Linux' ]; then
 	OS='Linux'
-elif [ "$(expr substr %(uname -s) 1 10)" == 'MINGW32_NT' ]; then
+elif [ "$(expr substr $(uname -s) 1 10)" = 'MINGW32_NT' ]; then
 	OS='Cygwin'
 else
 	echo "Unsupported platform: ($(uname -a))"
