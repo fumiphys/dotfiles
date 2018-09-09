019770246ee912f8c6f4c4256ca88d5986d101e6
fumiphys <ufimihot2703@gmail.com>
2018-09-09 15:42:20 +0900

    setting.sh: fix bug of setting.sh

--- setting.sh
+++ setting.sh
@@ -25,11 +25,11 @@ NPWD="$(cd $(dirname $0); pwd)"
 
 # check original .vimrc
 if [ -e ~/.vimrc ]; then
-	echo -n "file ~/.vimrc already exists. overwrite? [y/n] "
+	echo -n "configuration files ~/.vimrc already exists. overwrite? [y/n] "
   read VIMRC_YN
 
   case $VIMRC_YN in
-    "" | "Y" | "y" | "yes" | "Yes" | "YES" ) echo "~/.vimrc will be overwritten";;
+    "" | "Y" | "y" | "yes" | "Yes" | "YES" ) echo "configuration files will be overwritten";;
     * ) echo "stop configuration!"
         exit 1;;
   esac
@@ -37,13 +37,29 @@ fi
 
 
 # link configuration files
+if [ -e ~/.vimrc ]; then
+  rm ~/.vimrc
+fi
 ln -F -s ${NPWD}/vim/.vimrc ~/.vimrc
 mkdir -p ~/.vim/
 mkdir -p ~/.vim/conf.d
 mkdir -p ~/.vim/conf.d/bases
+
+if [ -e ~/.vim/conf.d/bases/base_options.vim ]; then
+  rm ~/.vim/conf.d/bases/base_options.vim
+fi
 ln -F -s ${NPWD}/vim/conf.d/bases/base_options.vim ~/.vim/conf.d/bases/base_options.vim
+
+if [ -e ~/.vim/conf.d/bases/keymap.vim ]; then
+  rm ~/.vim/conf.d/bases/keymap.vim
+fi
 ln -F -s ${NPWD}/vim/conf.d/bases/keymap.vim ~/.vim/conf.d/bases/keymap.vim
+
+if [ -e ~/.vim/conf.d/bases/gui.vim ]; then
+  rm ~/.vim/conf.d/bases/gui.vim
+fi
 ln -F -s ${NPWD}/vim/conf.d/bases/gui.vim ~/.vim/conf.d/bases/gui.vim
+
 mkdir -p ~/.vim/conf.d/languages
 mkdir -p ~/.vim/conf.d/plugins
 mkdir -p ~/.vim/bundle
