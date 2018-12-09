ca266ba638c6284b6bc48463600354ff02f855d3
fumiphys <ufimihot2703@gmail.com>
2018-12-09 20:40:18 +0900

    refactored setting.sh

--- setting.sh
+++ setting.sh
@@ -86,6 +86,154 @@ install_python() {
   pip3 install --upgrade jedi
 }
 
+link_vim_settings() {
+  # link configuration files
+  if [ -e ~/.vimrc ]; then
+    rm ~/.vimrc
+  fi
+  ln -F -s ${NPWD}/vim/.vimrc ~/.vimrc
+  mkdir -p ~/.vim/
+  mkdir -p ~/.vim/conf.d
+  mkdir -p ~/.vim/conf.d/bases
+
+  # base_options.vim
+  if [ -e ~/.vim/conf.d/bases/base_options.vim ]; then
+    rm ~/.vim/conf.d/bases/base_options.vim
+  fi
+  ln -F -s ${NPWD}/vim/conf.d/bases/base_options.vim ~/.vim/conf.d/bases/base_options.vim
+
+  # keymap.vim
+  if [ -e ~/.vim/conf.d/bases/keymap.vim ]; then
+    rm ~/.vim/conf.d/bases/keymap.vim
+  fi
+  ln -F -s ${NPWD}/vim/conf.d/bases/keymap.vim ~/.vim/conf.d/bases/keymap.vim
+
+  # gui.vim
+  if [ -e ~/.vim/conf.d/bases/gui.vim ]; then
+    rm ~/.vim/conf.d/bases/gui.vim
+  fi
+  ln -F -s ${NPWD}/vim/conf.d/bases/gui.vim ~/.vim/conf.d/bases/gui.vim
+
+  mkdir -p ~/.vim/conf.d/plugins
+
+  # dein_root.vim
+  if [ -e ~/.vim/conf.d/plugins/dein_root.vim ]; then
+    rm ~/.vim/conf.d/plugins/dein_root.vim
+  fi
+  ln -F -s ${NPWD}/vim/conf.d/plugins/dein_root.vim ~/.vim/conf.d/plugins/dein_root.vim
+
+  # dein.toml
+  if [ -e ~/.vim/conf.d/plugins/dein.toml ]; then
+    rm ~/.vim/conf.d/plugins/dein.toml
+  fi
+  ln -F -s ${NPWD}/vim/conf.d/plugins/dein.toml ~/.vim/conf.d/plugins/dein.toml
+
+  # dein_lazy.toml
+  if [ -e ~/.vim/conf.d/plugins/dein_lazy.toml ]; then
+    rm ~/.vim/conf.d/plugins/dein_lazy.toml
+  fi
+  ln -F -s ${NPWD}/vim/conf.d/plugins/dein_lazy.toml ~/.vim/conf.d/plugins/dein_lazy.toml
+
+  mkdir -p ~/.vim/conf.d/languages
+
+  # ccpp.vim
+  if [ -e ~/.vim/conf.d/languages/ccpp.vim ]; then
+    rm ~/.vim/conf.d/languages/ccpp.vim
+  fi
+  ln -F -s ${NPWD}/vim/conf.d/languages/ccpp.vim ~/.vim/conf.d/languages/ccpp.vim
+
+  # tex.vim
+  if [ -e ~/.vim/conf.d/languages/tex.vim ]; then
+    rm ~/.vim/conf.d/languages/tex.vim
+  fi
+  ln -F -s ${NPWD}/vim/conf.d/languages/tex.vim ~/.vim/conf.d/languages/tex.vim
+
+  # markdown.css
+  if [ -e ~/.vim/conf.d/languages/markdown.css ]; then
+    rm ~/.vim/conf.d/languages/markdown.css
+  fi
+  ln -F -s ${NPWD}/vim/conf.d/languages/markdown.css ~/.vim/conf.d/languages/markdown.css
+
+  mkdir -p ~/.vim/bundle
+}
+
+patch_markdown() {
+  # markdown
+  # this will be fixed if previm module has some changes
+  cd ~/.vim/bundle/repos/github.com/kannokanno/previm
+  git checkout .
+  patch -u preview/_/index.html < "${NPWD}/vim/conf.d/languages/mathjax_html.patch"
+  patch -u preview/_/js/previm.js < "${NPWD}/vim/conf.d/languages/mathjax_js.patch"
+  cd ${NPWD}
+}
+
+link_zsh() {
+  if [ ${WZSH} = "true" ]; then
+    set +e
+    if [ ${OS} = "Mac" ]; then
+      brew install peco
+    fi
+    set -e
+    # zsh
+    if [ -e ~/.zshrc ]; then
+      rm ~/.zshrc
+    fi
+    ln -F -s ${NPWD}/zsh/.zshrc ~/.zshrc
+    if [ ${WZPLUG} = "true" ]; then
+      curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
+    fi
+    mkdir -p ~/.zprezto
+    ln -sf ~/.zplug/repos/sorin-ionescu/prezto ~/.zprezto
+    if [ -e ~/.zlogin ]; then
+      rm ~/.zlogin
+    fi
+    ln -s ~/.zprezto/prezto/runcoms/zlogin ~/.zlogin
+    if [ -e ~/.zlogout ]; then
+      rm ~/.zlogout
+    fi
+    ln -s ~/.zprezto/prezto/runcoms/zlogout ~/.zlogout
+    if [ -e ~/.zpreztorc ]; then
+      rm ~/.zpreztorc
+    fi
+    ln -s ~/.zprezto/prezto/runcoms/zpreztorc ~/.zpreztorc
+    if [ -e ~/.zprofile ]; then
+      rm ~/.zprofile
+    fi
+    ln -s ~/.zprezto/prezto/runcoms/zprofile ~/.zprofile
+    if [ -e ~/.zshenv ]; then
+      rm ~/.zshenv
+    fi
+    ln -s ~/.zprezto/prezto/runcoms/zshenv ~/.zshenv
+  else
+    printf "\e[32mSkip configuration for zsh ...\e[m\n"
+  fi
+}
+
+link_tex() {
+  if [ -e ~/.latexmkrc ]; then
+    rm ~/.latexmkrc
+  fi
+  if [ ${OS} = "Mac" ]; then
+    set +e
+    brew install evince
+    set -e
+    # latexmkrc
+    ln -F -s ${NPWD}/latex/.latexmkrc.mac ~/.latexmkrc
+  elif [ ${OS} = "Linux" ]; then
+    # latexmkrc
+    sudo apt install mupdf
+    ln -F -s ${NPWD}/latex/.latexmkrc.linux ~/.latexmkrc
+  fi
+}
+
+config_github() {
+  if [ -e ~/.gitignore_global ]; then
+    rm ~/.gitignore_global
+  fi
+  ln -F -s ${NPWD}/github/.gitignore_global ~/.gitignore_global
+  git config --global core.excludesfile ~/.gitignore_global
+}
+
 # check os is valid
 check_os
 echo "OS: $OS"
@@ -102,151 +250,22 @@ ask_resetting
 printf "\e[32mInstalling python3 and depending packages ...\e[m\n"
 install_python
 
-exit 1
 
+# link vim settings
 printf "\e[32mConfiguration for vim ...\e[m\n"
-# link configuration files
-if [ -e ~/.vimrc ]; then
-  rm ~/.vimrc
-fi
-ln -F -s ${NPWD}/vim/.vimrc ~/.vimrc
-mkdir -p ~/.vim/
-mkdir -p ~/.vim/conf.d
-mkdir -p ~/.vim/conf.d/bases
+link_vim_settings
+patch_markdown
 
-# base_options.vim
-if [ -e ~/.vim/conf.d/bases/base_options.vim ]; then
-  rm ~/.vim/conf.d/bases/base_options.vim
-fi
-ln -F -s ${NPWD}/vim/conf.d/bases/base_options.vim ~/.vim/conf.d/bases/base_options.vim
+# link zsh
+printf "\e[32mConfiguration for zsh ...\e[m\n"
+link_zsh
 
-# keymap.vim
-if [ -e ~/.vim/conf.d/bases/keymap.vim ]; then
-  rm ~/.vim/conf.d/bases/keymap.vim
-fi
-ln -F -s ${NPWD}/vim/conf.d/bases/keymap.vim ~/.vim/conf.d/bases/keymap.vim
-
-# gui.vim
-if [ -e ~/.vim/conf.d/bases/gui.vim ]; then
-  rm ~/.vim/conf.d/bases/gui.vim
-fi
-ln -F -s ${NPWD}/vim/conf.d/bases/gui.vim ~/.vim/conf.d/bases/gui.vim
-
-mkdir -p ~/.vim/conf.d/plugins
-
-# dein_root.vim
-if [ -e ~/.vim/conf.d/plugins/dein_root.vim ]; then
-  rm ~/.vim/conf.d/plugins/dein_root.vim
-fi
-ln -F -s ${NPWD}/vim/conf.d/plugins/dein_root.vim ~/.vim/conf.d/plugins/dein_root.vim
-
-# dein.toml
-if [ -e ~/.vim/conf.d/plugins/dein.toml ]; then
-  rm ~/.vim/conf.d/plugins/dein.toml
-fi
-ln -F -s ${NPWD}/vim/conf.d/plugins/dein.toml ~/.vim/conf.d/plugins/dein.toml
-
-# dein_lazy.toml
-if [ -e ~/.vim/conf.d/plugins/dein_lazy.toml ]; then
-  rm ~/.vim/conf.d/plugins/dein_lazy.toml
-fi
-ln -F -s ${NPWD}/vim/conf.d/plugins/dein_lazy.toml ~/.vim/conf.d/plugins/dein_lazy.toml
-
-mkdir -p ~/.vim/conf.d/languages
-
-# ccpp.vim
-if [ -e ~/.vim/conf.d/languages/ccpp.vim ]; then
-  rm ~/.vim/conf.d/languages/ccpp.vim
-fi
-ln -F -s ${NPWD}/vim/conf.d/languages/ccpp.vim ~/.vim/conf.d/languages/ccpp.vim
-
-# tex.vim
-if [ -e ~/.vim/conf.d/languages/tex.vim ]; then
-  rm ~/.vim/conf.d/languages/tex.vim
-fi
-ln -F -s ${NPWD}/vim/conf.d/languages/tex.vim ~/.vim/conf.d/languages/tex.vim
-
-# markdown.css
-if [ -e ~/.vim/conf.d/languages/markdown.css ]; then
-  rm ~/.vim/conf.d/languages/markdown.css
-fi
-ln -F -s ${NPWD}/vim/conf.d/languages/markdown.css ~/.vim/conf.d/languages/markdown.css
-
-mkdir -p ~/.vim/bundle
-
-# markdown
-# this will be fixed if previm module has some changes
-cd ~/.vim/bundle/repos/github.com/kannokanno/previm
-git checkout .
-patch -u preview/_/index.html < "${NPWD}/vim/conf.d/languages/mathjax_html.patch"
-patch -u preview/_/js/previm.js < "${NPWD}/vim/conf.d/languages/mathjax_js.patch"
-cd ${NPWD}
-
-
-if [ ${WZSH} ]; then
-  printf "\e[32mConfiguration for zsh ...\e[m\n"
-  set +e
-  if [ ${OS} = "Mac" ]; then
-    brew install peco
-  fi
-  set -e
-  # zsh
-  if [ -e ~/.zshrc ]; then
-    rm ~/.zshrc
-  fi
-  ln -F -s ${NPWD}/zsh/.zshrc ~/.zshrc
-  if [ ${WZPLUG} ]; then
-    curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
-  fi
-  mkdir -p ~/.zprezto
-  ln -sf ~/.zplug/repos/sorin-ionescu/prezto ~/.zprezto
-  if [ -e ~/.zlogin ]; then
-    rm ~/.zlogin
-  fi
-  ln -s ~/.zprezto/prezto/runcoms/zlogin ~/.zlogin
-  if [ -e ~/.zlogout ]; then
-    rm ~/.zlogout
-  fi
-  ln -s ~/.zprezto/prezto/runcoms/zlogout ~/.zlogout
-  if [ -e ~/.zpreztorc ]; then
-    rm ~/.zpreztorc
-  fi
-  ln -s ~/.zprezto/prezto/runcoms/zpreztorc ~/.zpreztorc
-  if [ -e ~/.zprofile ]; then
-    rm ~/.zprofile
-  fi
-  ln -s ~/.zprezto/prezto/runcoms/zprofile ~/.zprofile
-  if [ -e ~/.zshenv ]; then
-    rm ~/.zshenv
-  fi
-  ln -s ~/.zprezto/prezto/runcoms/zshenv ~/.zshenv
-else
-  printf "\e[32mSkip configuration for zsh ...\e[m\n"
-fi
-
+# link tex
+link_tex
 printf "\e[32mConfiguration for tex ...\e[m\n"
-# tex
-if [ -e ~/.latexmkrc ]; then
-  rm ~/.latexmkrc
-fi
-if [ ${OS} = "Mac" ]; then
-  set +e
-  brew install evince
-  set -e
-  # latexmkrc
-  ln -F -s ${NPWD}/latex/.latexmkrc.mac ~/.latexmkrc
-elif [ ${OS} = "Linux" ]; then
-  # latexmkrc
-  sudo apt install mupdf
-  ln -F -s ${NPWD}/latex/.latexmkrc.linux ~/.latexmkrc
-fi
 
+# config github
 printf "\e[32mConfiguration for github ...\e[m\n"
-# github
-if [ -e ~/.gitignore_global ]; then
-  rm ~/.gitignore_global
-fi
-ln -F -s ${NPWD}/github/.gitignore_global ~/.gitignore_global
-git config --global core.excludesfile ~/.gitignore_global
+config_github
 
 printf "\e[32mDone!\e[m\n"
