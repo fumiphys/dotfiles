78ac774cb5a2c197aa707d434446e8767e9fccd7
fumiphys <ufimihot2703@gmail.com>
2018-12-09 19:30:10 +0900

    setting.sh: add check_os func

--- setting.sh
+++ setting.sh
@@ -2,23 +2,29 @@
 # written by fumiphys
 # To use this script, execute
 # $ chmod +x setting.sh
-# $ ./setting.sh
+# $ ./setting.sh [--with-zsh, --with-zplug]
 
 # os information
 # if you use mac, brew will be used.
 # if you use linux, apt will be used.
 # if you use windows, I will think it later.
-if [ "$(uname)" = 'Darwin' ]; then
-	OS="Mac"
-elif [ "$(expr substr $(uname -s) 1 5)" = 'Linux' ]; then
-	OS='Linux'
-elif [ "$(expr substr $(uname -s) 1 10)" = 'MINGW32_NT' ]; then
-	OS='Cygwin'
-else
-	echo "Unsupported platform: ($(uname -a))"
-	echo "Supported platform is: [Mac, Linux, Cygwin]"
-	exit 1
-fi
+check_os() {
+  if [ "$(uname)" = 'Darwin' ]; then
+    OS="Mac"
+  elif [ "$(expr substr $(uname -s) 1 5)" = 'Linux' ]; then
+    OS='Linux'
+  elif [ "$(expr substr $(uname -s) 1 10)" = 'MINGW32_NT' ]; then
+    OS='Cygwin'
+  else
+    echo "Unsupported platform: ($(uname -a))"
+    echo "Supported platform is: [Mac, Linux, Cygwin]"
+    exit 1
+  fi
+}
+
+# check os is valid
+check_os
+echo "OS: $OS"
 
 for opt in "$@"; do
   case "${opt}" in
